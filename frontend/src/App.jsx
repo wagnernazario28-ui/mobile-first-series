c0e8cd2792041aced9322d5cb50a6283aef83747
wagnernazario28-ui <wagnernazario28@gmail.com>
2025-09-21 21:58:19 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>Mobile-First Series</h1>
+        <p>Seu Guia de Séries Personalizado</p>
+      </header>
+      <main className="app-content">
+        {/* Futuramente, aqui entrarão os componentes de seleção de séries */}
+        <h2>Em construção...</h2>
+      </main>
+    </div>
+  )
+}
+
+export default App
